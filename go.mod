c2bae5f855a1e8f8a494361815d30857dea26578
xzjianyu <xzjianyu@xzjianyu.lan>
2019-10-28 15:56:20 +0800

    test ipfs core api

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/jian-yu/ipfs-connector
+
+go 1.12
+
+require (
+	github.com/ipfs/go-ipfs-http-client v0.0.5
+	github.com/ipfs/go-ipld-format v0.0.2
+	github.com/ipfs/interface-go-ipfs-core v0.2.3
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/multiformats/go-multiaddr v0.1.1
+	google.golang.org/appengine v1.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	ipld "github.com/ipfs/go-ipld-format"
+	"github.com/ipfs/interface-go-ipfs-core/path"
+
+	ipfs "github.com/ipfs/go-ipfs-http-client"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+const (
+	ipfsGateway = "/ip4/172.38.8.89/tcp/9090/ipfs/QmcPVRJAKSQA17bH2kiBhCaFfQ5cHGjEBraD71VdGgSBYt"
+)
+
+func main() {
+	Get("/ipfs/QmUCNsMiSFM2p1LnHpWCwFrPuWqcPnAZquvsgPinHWxBTc")
+}
+
+func Get(ipfsPath string) {
+	ctx := context.Background()
+	multiAddr, err := ma.NewMultiaddr(ipfsGateway)
+	if err != nil {
+		log.Fatalf("generate multiaddr error by %s", err)
+	}
+	ipfsApi, err := ipfs.NewApi(multiAddr)
+	if err != nil {
+		log.Fatalf("ipfs http connect error by %s", err)
+	}
+	srcPath := path.New(ipfsPath)
+	nodeObject, err := ipfsApi.Object().Get(ctx, srcPath)
+	if err != nil {
+		log.Fatalf("get object node fail by %s", err)
+	}
+	data, err := GetOne(ctx, nodeObject, ipfsApi)
+	if err != nil {
+		log.Fatalf("get object fail by %s", err)
+	}
+	fmt.Println(string(data))
+}
+
+func GetOne(ctx context.Context, node ipld.Node, api *ipfs.HttpApi) ([]byte, error) {
+	var err error
+	data := make([]byte, 0)
+	data = append(data, node.RawData()[:]...)
+	if len(node.Links()) == 0 {
+		return data, err
+	}
+	for _, link := range node.Links() {
+		srcPath := path.New(link.Cid.String())
+		node, err := api.Object().Get(ctx, srcPath)
+		if err != nil {
+			log.Fatalf("get object link fail by %s", err)
+		}
+		data, err = GetOne(ctx, node, api)
+		if err != nil {
+			log.Fatalf("get link node fail by %s", err)
+			break
+		}
+		data = append(data, data[:]...)
+	}
+	return data, err
+}
